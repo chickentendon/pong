c4ece7ff291e015a4dd15a6ce04c8ea84464db72
Hoefs014 <hoefs014@d.umn.edu>
2015-11-29 16:01:16 -0600

    Updated Paddles and KeyListener

--- Pong/src/GamePanel.java
+++ Pong/src/GamePanel.java
@@ -8,7 +8,7 @@ import java.awt.event.KeyListener;
 import javax.swing.JPanel;
 import javax.swing.Timer;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements KeyListener {
     private Pong game;
     private Ball ball;
     private Paddle player1, player2;
@@ -19,12 +19,22 @@ public class GamePanel extends JPanel {
         setForeground(Color.BLACK);
         this.game = game;
         //ball = new Ball(game);
-        player1 = new Paddle(20, 20, 20);
-        player2 = new Paddle(Pong.WIDTH - 40, 20, 20);
+        player1 = new Paddle(20, 87, 83);
+        player2 = new Paddle(Pong.WIDTH - 40, 38, 40);
         //Timer timer = new Timer(5, this);
         //timer.start();
-        //addKeyListener(this);
+        addKeyListener(this);
         setFocusable(true);
+    }
+    
+    public void keyPressed(KeyEvent e){
+    	player1.pressed(e.getKeyCode());
+    	player2.pressed(e.getKeyCode());
+    }
+    
+    public void keyReleased(KeyEvent e){
+    	player1.released(e.getKeyCode());
+    	player2.released(e.getKeyCode());
     }
     
     @Override
@@ -32,5 +42,10 @@ public class GamePanel extends JPanel {
     	super.paintComponent(g);
     	player1.paint(g);
     	player2.paint(g);
-    }    
+    }
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		//Unused
+	}    
 }
--- Pong/src/Paddle.java
+++ Pong/src/Paddle.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
 
 
 public class Paddle {
@@ -21,6 +22,25 @@ public class Paddle {
 		System.out.println(y);
 	}
 	
+	public void update(){
+		
+		
+		//repaint();
+	}
+	
+	public void pressed(int code){
+		if(code == up){
+			y += 1; 
+		} else if (code == down){
+			y -= 1;
+		}
+	}
+	
+	public void released(KeyEvent e){
+		
+		
+	}
+
 	private int getHeight(){
 		return height;
 	}
